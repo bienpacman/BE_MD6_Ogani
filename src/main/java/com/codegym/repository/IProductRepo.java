21b2f6b50a0974e7c01e5658be6ec329d40d4a55
aittuandung <tuandung.ait.it@gmail.com>
2022-09-05 08:34:11 +0700

    api, repo, service cho Product

--- src/main/java/com/codegym/repository/IProductRepo.java
+++ src/main/java/com/codegym/repository/IProductRepo.java
@@ -1,4 +1,12 @@
 package com.codegym.repository;
 
-public interface IProductRepo {
+import com.codegym.model.Product;
+import com.codegym.model.Seller;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface IProductRepo extends CrudRepository<Product, Long> {
+    List<Product> getAllBySeller(Seller seller);
 }
--- src/main/java/com/codegym/service/ProductService.java
+++ src/main/java/com/codegym/service/ProductService.java
@@ -1,4 +1,32 @@
 package com.codegym.service;
 
+import com.codegym.model.Product;
+import com.codegym.model.Seller;
+import com.codegym.repository.IProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    @Autowired
+    IProductRepo iProductRepo;
+
+    public List<Product> getAllBySeller(Seller seller){
+        List<Product> products = iProductRepo.getAllBySeller(seller);
+        return products;
+    }
+
+    public void save(Product product){
+        iProductRepo.save(product);
+    }
+
+    public void delete(long id){
+        iProductRepo.deleteById(id);
+    }
+
+    public Product findById(long id){
+        return iProductRepo.findById(id).get();
+    }
 }
