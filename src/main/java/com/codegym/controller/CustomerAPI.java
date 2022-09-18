2795545dbeffbbce54b218bb0162fdf85f9951d5
vu viet <vvviet97@gmail.com>
2022-09-18 17:50:55 +0700

    comment product

--- src/main/java/com/codegym/controller/CustomerAPI.java
+++ src/main/java/com/codegym/controller/CustomerAPI.java
@@ -1,13 +1,18 @@
 package com.codegym.controller;
 
 import com.codegym.model.Customer;
+import com.codegym.model.ProductComment;
 import com.codegym.model.Seller;
 import com.codegym.service.CustomerService;
+import com.codegym.service.ProductCommentService;
 import com.codegym.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -20,6 +25,9 @@ public class CustomerAPI {
 
     ProductService productService;
 
+    @Autowired
+    ProductCommentService productCommentService;
+
     @PostMapping("/findCustomerByUserName")
     public ResponseEntity<Customer> findCustomerByUserID(@RequestBody String userName){
         Customer customer = customerService.findCustomerByUserName(userName);
@@ -29,6 +37,19 @@ public class CustomerAPI {
     @PostMapping("/findSellerByProductId")
     public ResponseEntity<Seller> findSellerByProductId(@RequestBody Long id){
         return new ResponseEntity<Seller>(productService.findProductById(id).getSeller(), HttpStatus.OK);
+    }
+
+    @PostMapping("/save-productComment")
+    public ResponseEntity<ProductComment> save(@RequestBody ProductComment productComment){
+        java.util.Date now = new java.util.Date();
+        productComment.setCreatAt(now);
+        return new ResponseEntity<ProductComment>(productCommentService.save(productComment), HttpStatus.OK);
+    }
+
+    @PostMapping("/findProductCommentListByProductId")
+    public ResponseEntity<List<ProductComment>> findProductCommentListByProductId(@RequestBody Long idProduct){
+
+        return new ResponseEntity<List<ProductComment>>(productCommentService.findProductCommentListByProductId(idProduct), HttpStatus.OK);
     }
 
 }
